58eed664fe40e32bed993dc3cb788257f950c8f1
Martina Dimitrova <martina.dimitrova22@gmail.com>
2018-03-28 10:41:18 +0300

    Changes in indexController.php and gumiController

--- Controller/gumiController.php
+++ Controller/gumiController.php
@@ -9,23 +9,17 @@ if($_SESSION["logged_user"]) {
         $price = $_POST["hidden_price"];
         $quantity = 1;
 
-        $cart=array();
         $product = array();
-        $product = ["name_brand" => $name_brand, "season" => $season, "size" => $size, "quantity" => $quantity, "price" => $price];
+        $product[]
+
+        $_SESSION["cart"]=array();
+        $_SESSION["cart"]["name_brand"] = array("season" => $season, "size" => $size, "quantity" => $quantity, "price" => $price);
 
 
-        $cart = $_SESSION["cart"];
 
-    foreach ($cart as $guma) {
-        if (($guma["name_brand"]) == $name_brand) {
-        $guma[$quantity]++;
-        } else {
-        $product = ["name_brand" => $name_brand, "season" => $season, "size" => $size, "quantity" => $quantity, "price" => $price];
-    }
     }
 
-        $cart[]= $product;
-        $_SESSION["cart"] = $cart;
+
 
         require_once "../Model/userDao.php";
         buyTire($_POST["hidden_tire"]);
@@ -40,16 +34,47 @@ else{
 
 
 }
-if(isset($_POST["delete"])){
+if(isset($_POST["delete"])) {
     $deleted_pr_name = $_POST["name_tire"];
 
-    foreach ($_SESSION["cart"] as $item){
-    if(isset($item[$deleted_pr_name])) { //ne raboti
+    var_dump($deleted_pr_name);
+    var_dump($_SESSION["cart"]);
 
-        $product_data = &$_SESSION["cart"][$deleted_pr_name];
-        unset($_SESSION["cart"][$deleted_pr_name]);
-    }
-    }
-  header("Location:../View/cart.php");
+    for ($i=0; $i<count($_SESSION["cart"]); $i++){
+       echo "tova e guma: ";
 
+        $guma = array();
+        $guma = [$i];
+        var_dump($guma);
+        foreach ($guma as $value ){
+            echo"tova e value:";
+            var_dump($value) ;
+            if($value == $deleted_pr_name){
+                echo "tova,koeto iskame da unsetnem:".$_SESSION["cart"][$i];
+                unset($_SESSION["cart"][$i]);
+
+                echo "sesia sled unset: ";
+                var_dump($_SESSION["cart"]);
+            }
+        }
+    }
+
+//    echo $deleted_pr_name . "ime na produkta ot delete";
+//    $max = count($_SESSION["cart"]);
+//    echo $max . " tova e max";
+//    for ($i = 0; $i < $max; $i++) {
+//        var_dump($i);
+//
+//        if (($i["name_brand"]) == $deleted_pr_name) {
+//            echo $i["name_brand"] . "ime na produkta ot sesiqta, veroqtno"; //ne raboti
+//            unset($_SESSION["cart"][$i]);
+//
+//            echo "!!!!!!";
+//            var_dump($_SESSION["cart"]);
+//            echo "tova e sesiqta sled unset";
+//        }
+//        //header("Location:../View/cart.php");
+//
+//
+//    }
 }
